8c466461204f7bebdff27703a9015b69365fe07f
Felipe Teles <felipe.p.teles@hotmail.com>
2025-10-14 19:38:31 -0300

    Alterado schema do telefone

--- routes/telefones.ts
+++ routes/telefones.ts
@@ -7,8 +7,8 @@ const prisma = new PrismaClient()
 const router = Router()
 
 const telefonesSchema = z.object({
-    tel1: z.string().min(10).max(11, {message: "O telefone deve possuir, no mínimo 11 caracteres e no máximo 11 caracteres."}),
-    tel2: z.string().min(10).max(11, {message: "O telefone deve possuir, no mínimo 11 caracteres e no máximo 11 caracteres."}).optional(),
+    tel1: z.string().min(10).max(11, {message: "O telefone deve possuir, no mínimo 10 caracteres e no máximo 11 caracteres."}),
+    tel2: z.string().min(10).max(11, {message: "O telefone deve possuir, no mínimo 10 caracteres e no máximo 11 caracteres."}).optional(),
     clienteId: z.string().uuid()
 })
 
